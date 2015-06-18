8285b5cea71f725dd496dcaf9d69d8d2995ca361
rtregaskis <rtregaskis@dresources.com>
2015-06-18 09:41:30 +0100

    new jsbeautifyrc

--- setup.sh
+++ setup.sh
@@ -9,3 +9,4 @@ ln -s $HERE/bash_aliases ~/.bash_aliases
 ln -s $HERE/bash_profile ~/.bash_profile
 ln -s $HERE/vimrc ~/.vimrc
 ln -s $HERE/jshintrc ~/.jshintrc
+ln -s $HERE/jsbeautifyrc ~/.jsbeautifyrc
