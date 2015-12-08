f7341ba916b0843a955be2c50cb389c4b8777254
rtregaskis <rtregaskis@dresources.com>
2015-12-08 10:11:06 +0000

    update: rename source files to correct filetype

--- setup.sh
+++ setup.sh
@@ -8,9 +8,9 @@ ln -s $HERE/bashrc ~/.bashrc
 ln -s $HERE/bash_aliases ~/.bash_aliases
 ln -s $HERE/bash_profile ~/.bash_profile
 ln -s $HERE/vimrc ~/.vimrc
-ln -s $HERE/jshintrc ~/.jshintrc
-ln -s $HERE/htmlhintrc ~/.htmlhintrc
-ln -s $HERE/jscsrc ~/.jscsrc
+ln -s $HERE/jshintrc.json ~/.jshintrc
+ln -s $HERE/htmlhintrc.json ~/.htmlhintrc
+ln -s $HERE/jscsrc.json ~/.jscsrc
 ln -s $HERE/jsbeautifyrc ~/.jsbeautifyrc
 ln -s $HERE/gitconfig ~/.gitconfig
 ln -s $HERE/scss-lint.yml ~/.scss-lint.yml
