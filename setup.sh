5dc812be74d7a9e5978d8e61c434eabfb540db0d
rtregaskis <rtregaskis@dresources.com>
2015-12-09 10:43:29 +0000

    update: file extension of jsbeautifyrc

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ ln -s $HERE/vimrc ~/.vimrc
 ln -s $HERE/jshintrc.json ~/.jshintrc
 ln -s $HERE/htmlhintrc.json ~/.htmlhintrc
 ln -s $HERE/jscsrc.json ~/.jscsrc
-ln -s $HERE/jsbeautifyrc ~/.jsbeautifyrc
+ln -s $HERE/jsbeautifyrc.json ~/.jsbeautifyrc
 ln -s $HERE/gitconfig ~/.gitconfig
 ln -s $HERE/scss-lint.yml ~/.scss-lint.yml
 ln -s $HERE/alias_completion.sh ~/.alias_completion.sh
