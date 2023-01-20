4041cf350150546efd03afab85df69e313cf341a
Robin Tregaskis <lokidokicoki@gmail.com>
2023-01-20 18:06:47 +0000

    feat(install_apps.sh): use apt to install default packages. install Vundle, compile YCM

---
+++ install_apps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Install apps"
+
+sudo apt update
+
+sudo apt install fortune-mod vim-gtk3 vim-nox vim-doc python3-dev python-is-python3 git build-essential cmake golang nodejs npm mono-complete openjdk-17-jdk openjdk-17-jre
+
+
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+	echo "Install Vundle and vim plugins"
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	vim.nox  +PluginInstall +qall
+fi
+
+echo "Build YCM"
+
+cd $HOME/.vim/bundle/YouCompleteMe
+python3 install.py --all
+
+
+echo "Install App complete!"
