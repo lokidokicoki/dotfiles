e5bfa4071acc7526c14a09b0ddf7283e76d8f571
Robin Tregaskis <lokidokicoki@gmail.com>
2023-01-22 15:46:11 +0000

    fix(install_apps): include openssh-server and start service

--- install_apps.sh
+++ install_apps.sh
@@ -4,7 +4,7 @@ echo "Install apps"
 
 sudo apt update
 
-sudo apt install fortune-mod vim-gtk3 vim-nox vim-doc python3-dev python-is-python3 git build-essential cmake golang nodejs npm mono-complete openjdk-17-jdk openjdk-17-jre
+sudo apt install fortune-mod vim-gtk3 vim-nox vim-doc python3-dev python-is-python3 git build-essential cmake golang nodejs npm mono-complete openjdk-17-jdk openjdk-17-jre openssh-server
 
 
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
@@ -17,6 +17,9 @@ echo "Build YCM"
 
 cd $HOME/.vim/bundle/YouCompleteMe
 python3 install.py --all
+
+echo "Setup SSH server"
+sudo systemctl enable ssh --now
 
 
 echo "Install App complete!"
