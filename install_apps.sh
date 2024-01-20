3aa17b55ef8e381cb5db88096bd2d38b5b7edb1e
Robin Tregaskis <lokidokicoki@gmail.com>
2024-01-20 09:12:01 +0000

    chore(install_apps): cleaner way to define apps

--- install_apps.sh
+++ install_apps.sh
@@ -2,9 +2,29 @@
 
 echo "Install apps"
 
-sudo apt update
+APPS=(
+	"fortune-mod"
+	"vim-gtk3"
+	"vim-nox"
+	"vim-doc"
+	"python3-dev"
+	"python-is-python3"
+	"git"
+	"build-essential"
+	"cmake"
+	"golang"
+	"nodejs"
+	"npm"
+	"mono-complete"
+	"openjdk-17-jdk"
+	"openjdk-17-jre"
+	"openssh-server"
+	"fonts-powerline"
+	"neovim"
+)
 
-sudo apt install fortune-mod vim-gtk3 vim-nox vim-doc python3-dev python-is-python3 git build-essential cmake golang nodejs npm mono-complete openjdk-17-jdk openjdk-17-jre openssh-server fonts-powerline neovim
+sudo apt update
+sudo apt install "${APPS[@]}"
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
